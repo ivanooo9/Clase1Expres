48224e0f1f1f3a81106e9fe66e93b7c27c9380eb
ivanooo9 <marcoeli098@gmail.com>
2024-06-12 23:28:37 -0500

    FIX

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('../Clase1Expres/src/routes/index');
+var usersRouter = require('../Clase1Expres/src/routes/users');
 
 var app = express();
 
